99290a142f47c653fb922348871bfb5bdc69f846
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-10-18 19:35:55 -0400

    Added powerful authentication

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {Auth} from 'aws-amplify';
 
 // pages
 import Home from './pages/LandingPage';
-import Auth from './pages/Auth';
+//import Auth from './pages/Auth';
 import Launches from './pages/Launches';
+import MyAuth from './pages/MyAuth';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/login' component={Auth}/>
+          <Route exact path='/' component={MyAuth}/>
+          <Route path='/login' component={Home}/>
         </Switch>
       </Router>
     );
---
+++ spacex/src/pages/MyAuth.js
@@ -0,0 +1,111 @@
+import React, {useState, useEffect} from 'react';
+import {Auth, Hub} from 'aws-amplify';
+
+const initialFormState = {
+  username: '', password: '', email: '', authCode: '', formType: 'signUp'
+}
+
+function MyAuth() {
+  const [formState, updateFormState] = useState(initialFormState);
+  const [user, updateUser] = useState(null)
+
+  useEffect(()=> {
+    checkUser()
+    setAuthListener()
+  }, [])
+
+  async function setAuthListener() {
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signOut':
+          console.log('user signed out');
+          updateFormState(() => ({...formState, formType: "signUp"}));
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  async function checkUser() {
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      console.log('user: ', user)
+      updateUser(user)
+      updateFormState(() => ({...formState, formType: "signedIn" }))
+    } catch (err) {
+      updateUser(null)
+    }
+  }
+
+  function onChange(e) {
+    e.persist();
+    updateFormState(() => ({...formState, [e.target.name]: e.target.value}));
+  }
+  const { formType } = formState
+
+  async function signUp() {
+    const {username, email, password} = formState
+    await Auth.signUp({ username, password, attributes: {email}})
+    updateFormState(() => ({ ...formState, formType: "confirmSignUp" }))
+  }
+
+  async function confirmSignUp() {
+    const {username, authCode} = formState
+    await Auth.confirmSignUp(username, authCode)
+    updateFormState(() => ({ ...formState, formType: "signIn" }))
+  }
+
+  async function signIn() {
+    const {username, password} = formState
+    await Auth.signIn(username, password)
+    updateFormState(() => ({ ...formState, formType: "signedIn" }))
+  }
+
+  return (
+    <div className="App">
+      {
+        formType==='signUp' && (
+          <div>
+            <input name="username" onChange={onChange} placeholder="username" />
+            <input name="password" type="password" onChange={onChange} placeholder="password" />
+            <input name="email" onChange={onChange} placeholder="email" />
+            <button onClick={signUp}>Sign Up</button>
+            <button onClick={() => updateFormState(() => ({
+              ...formState, formType: "signIn"
+            }))}>Sign In</button>
+          </div>
+        )
+      }
+      {
+        formType==='confirmSignUp' && (
+          <div>
+            <input name="" onChange={onChange} placeholder="username" />
+            <input name="authCode" onChange={onChange} placeholder="Confirmation code" />
+            <button onClick={confirmSignUp}>Confirm Sign Up</button>
+          </div>
+        )
+      }
+      {
+        formType==='signIn' && (
+          <div>
+            <input name="username" onChange={onChange} placeholder="username" />
+            <input name="password" type="password" onChange={onChange} placeholder="password" />
+            <button onClick={signIn}>Sign In</button>
+          </div>
+        )
+      }
+      {
+        formType==='signedIn' && (
+          <div>
+            <button onClick={
+              () => Auth.signOut()
+            }>Sign Out</button>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default MyAuth;
